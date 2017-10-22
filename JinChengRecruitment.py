2764cdcd8ead081dac0d7debdbd2394097b8ddec
maicius <maicius@outlook.com>
2017-10-22 21:50:42 +0800

    update readme

---
+++ JinChengRecruitment.py
@@ -0,0 +1,29 @@
+# coding = utf-8
+import requests
+from bs4 import BeautifulSoup
+
+from Util import connect_redis
+
+#获取四川大学锦城学院就业信息
+def get_jincheng_recruit():
+
+    base_url = "http://www.scujcc.com.cn/channels/229"
+    req = requests.Session()
+    content = req.get(base_url + ".html").content.decode("utf-8")
+    re = connect_redis()
+    parse_jincheng(content, re)
+    for i in range(2, 99):
+        print(i)
+        url = base_url + "_" + str(i) + ".html"
+        content = req.get(url).content.decode("utf-8")
+        parse_jincheng(content, re)
+    print("finish")
+
+
+def parse_jincheng(content, re):
+    soup = BeautifulSoup(content, "html5lib")
+    infos = soup.find_all("span")
+    for i in range(2, 31, 2):
+        info = {'time': infos[i + 1].text, 'company': infos[i].text}
+        print(info)
+        re.lpush("jincheng_company_info", info)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from JinChengRecruitment import get_jincheng_recruit
 from SCURecruitment import get_scu_recruit
 from TsingHuaRecruitment import get_tsinghua_recruit
 
 if __name__ == '__main__':
-    get_scu_recruit()
-    get_tsinghua_recruit()
+    # get_scu_recruit()
+    # get_tsinghua_recruit()
+    get_jincheng_recruit()
