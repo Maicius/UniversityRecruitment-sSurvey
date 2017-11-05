f7edbf70b5573ecda6bde618a6d7f15a93b1d817
maicius <maicius@outlook.com>
2017-11-06 00:01:56 +0800

    添加中央财经大学数据

--- university/SUFERescruitment.py
+++ university/SUFERescruitment.py
@@ -1,21 +1,37 @@
 # coding=utf-8
+import json
+
 import requests
 
+from jedis import jedis
 from util import util
 
 
 def get_sufe_recruit():
     host = "career.sufe.edu.cn/"
     headers = util.get_header(host)
-    url = "http://careersys.sufe.edu.cn/pros_jiuye/s/zxh/owebsiteData/recruitmentAndPreaching?callback=&type=list&eachPageRows=500&currentPageno=1&_="
+    re = jedis.jedis()
+    re.connect_redis()
+
+    url = "http://careersys.sufe.edu.cn/pros_jiuye/s/zxh/owebsiteData/recruitmentAndPreaching?callback=&type=list&eachPageRows=413&currentPageno=1&_="
     req = requests.Session()
     res = req.get(headers=headers, url=url)
     content = res.content.decode("utf-8")
-    parse_info(content)
+    parse_info(content, re)
 
 
-def parse_info(content):
+def parse_info(content, re):
     print(content)
+    length = len(content)
+    info = content[1: length - 1]
+    company_dict = json.loads(info)
+    i = 0
+    for item in company_dict['listData']:
+        print(i)
+        company_name = item['yrdwname']
+        date = item['apjssj'][:10]
+        i +=1
+        re.save_info("sufe_company_info", date, company_name)
 
 
 
