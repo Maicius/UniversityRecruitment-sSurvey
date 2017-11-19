b55114bd5054f0cc356b94c6e0d217fd956f2a77
maicius <maicius@outlook.com>
2017-11-20 00:43:33 +0800

    分词

--- analysis/countTop500.py
+++ analysis/countTop500.py
@@ -1,7 +1,7 @@
 import json
 
 from jedis import jedis
-
+import jieba
 
 class AnalysisTop500(object):
     def __init__(self):
@@ -40,15 +40,33 @@ class AnalysisTop500(object):
                 continue
 
     def get_china_top_500(self, university):
-        university_company_list = self.re.lrange(university, 0, -1)
-        self.get_top_500_list()
-        for university in university_company_list:
-            if university in self.China_company_list:
-                print(university)
+        university_list = self.re.lrange(university, 0, -1)
 
+        self.get_top_500_list()
+        self.get_university_company_name(university_list)
+        for university in university_list:
+            if university in self.China_company_list:
+                pass
+
+    def get_university_company_name(self, university_list):
+        for university in university_list:
+            self.get_company_short_name(university)
+
+    def get_company_short_name(self, university):
+        company_list = self.re.lrange(university, 0, -1)
+        for company in company_list:
+            try:
+                company = company.replace('\'', '"')
+                company = json.loads(company)
+                company_name = company['company_name']
+                short_names = jieba.cut(company_name, cut_all=False)
+                print("===============")
+                print(company_name + ":" + " ".join(short_names))
+            except BaseException as e:
+                print(company)
+                continue
 
 if __name__ == '__main__':
     analysis = AnalysisTop500()
-    university_list = analysis.get_university_list()
     analysis.get_top_500_list()
-    print(" ".join(university_list))
+    analysis.get_china_top_500("university")
