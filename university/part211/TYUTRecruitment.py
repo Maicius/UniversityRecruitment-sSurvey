5573dc7121cde4b1477f6757ae458d4a88d11e98
WallfacerRZD <1048058400@qq.com>
2017-12-06 23:10:07 +0800

    太原工业大学

---
+++ university/part211/TYUTRecruitment.py
@@ -0,0 +1,81 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+from jedis import jedis
+import datetime
+from time import sleep
+
+
+def get_default_session():
+    session = requests.session()
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.91 Safari/537.36',
+    }
+    session.headers.update(header)
+    return session
+
+
+def parse_page(content, redis, table_name):
+    soup = BeautifulSoup(content, 'html5lib')
+    node_attrs = {
+        'id': "GridView1",
+    }
+    node = soup.find('table', attrs=node_attrs)
+    if node:
+        trs = node.find_all('tr')[1:]
+        for tr in trs:
+            tds = tr.find_all('td')
+            company_name = tds[0].find('a').text.strip()
+            date = tds[1].text.strip()[0:10]
+            redis.save_dict(table_name, dict(
+                company_name=company_name,
+                date=date,
+            ))
+    else:
+        pass
+
+
+def parse_json(content, redis, table_name):
+    json_data = json.loads(content)
+    if json_data:
+        json_data = json_data['data']
+        for item in json_data:
+            company_name = item['company_name']
+            date = item['meet_day']
+            redis.save_dict(table_name, dict(
+                company_name=company_name,
+                date=date,
+            ))
+    else:
+        pass
+
+
+def get_data(page, redis, table_name, s):
+
+    url = 'http://jiuye.tyut.edu.cn/zpweb/RecruitmentSpecial.aspx?Day=all&page=%d' % page
+    response = s.get(url)
+    content = response.content
+    parse_page(content, redis, table_name)
+
+
+def get_tyut_recruitment():
+    # 太原理工大学
+    table_name = 'tyut_company_info'
+
+    redis = jedis.jedis()
+    redis.clear_list(table_name)
+
+    session = get_default_session()
+    max_page = 165
+    try:
+        for i in range(1, max_page):
+            get_data(i, redis, table_name, session)
+            print('page ' + str(i) + ' done!')
+    except Exception as e:
+        redis.handle_error(e, table_name)
+    redis.add_to_file(table_name)
+    redis.add_university(table_name)
+
+
+if __name__ == '__main__':
+    get_tyut_recruitment()
