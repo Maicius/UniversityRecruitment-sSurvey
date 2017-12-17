e4bf14688198e2ece39582cc547fd97b4b5d4839
mai ciu s <maicius@outlook.com>
2017-12-17 23:29:40 +0800

    划分大学

--- analysis/SmartAnalysisByAddr.py
+++ analysis/SmartAnalysisByAddr.py
@@ -22,32 +22,35 @@ class SmartAnalysisByAddr(SmartAnalysisByName):
             university_province = UNIVERSITY_INFO[university_short_name][2]
             university_city = UNIVERSITY_INFO[university_short_name][3]
             university_type = UNIVERSITY_INFO[university_short_name][1]
+            university_name = UNIVERSITY_INFO[university_short_name][0]
             print(index, value)
             if university_province in self.p211_up_company_province and university_type in self.p211_up:
-                self.p211_up_company_province[university_province] += value
+                self.p211_up_company_province[university_province].append((university_name, value))
             elif university_province not in self.p211_up_company_province and university_type in self.p211_up:
-                self.p211_up_company_province[university_province] = value
+                self.p211_up_company_province[university_province] = [(university_name, value)]
 
             elif university_province in self.top_basic_company_province and university_type in self.basic_top:
-                self.top_basic_company_province[university_province] += value
+                self.top_basic_company_province[university_province].append((university_name, value))
 
-            elif university_province not in self.top_basic_company_province and university_province in self.basic_top:
-                self.top_basic_company_province[university_province] = value
+            elif university_province not in self.top_basic_company_province and university_type in self.basic_top:
+                self.top_basic_company_province[university_province] = [(university_name, value)]
 
             if university_type in self.p211_up:
                 if university_city in self.p211_up_company_city:
-                    self.p211_up_company_city[university_city] += value
+                    self.p211_up_company_city[university_city].append((university_name, value))
                 else:
-                    self.p211_up_company_city[university_city] = value
+                    self.p211_up_company_city[university_city] = [(university_name, value)]
             elif university_type in self.basic_top:
                 if university_city in self.top_basic_company_city:
-                    self.top_basic_company_city[university_city] += value
+                    self.top_basic_company_city[university_city].append((university_name, value))
                 else:
-                    self.top_basic_company_city[university_city] = value
+                    self.top_basic_company_city[university_city] = [(university_name, value)]
 
 
 if __name__ == '__main__':
     analysis = SmartAnalysisByAddr()
     analysis.calculate_num_by_addr()
-    print(analysis.p211_up_company_province)
-    print(analysis.top_basic_company_province)
+    print(sorted(analysis.p211_up_company_province.items(), key=lambda x: x[1], reverse=True))
+    print(sorted(analysis.p211_up_company_city.items(), key=lambda x: x[1], reverse=True))
+    print(sorted(analysis.top_basic_company_province.items(), key=lambda x: x[1], reverse=True))
+    print(sorted(analysis.top_basic_company_city.items(), key=lambda x: x[1], reverse=True))
